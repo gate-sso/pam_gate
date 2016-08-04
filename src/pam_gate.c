318dd1c2cef7c376190890dc8c94bbe2dd89a897
Ajey Gore <ajeygore@gmail.com>
2016-08-04 14:38:26 +0530

    Fixing typo

--- src/pam_gate.c
+++ src/pam_gate.c
@@ -217,7 +217,7 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 }
 
 static char *get_first_pass(pam_handle_t *pamh) {
-    void *password = NULL;
+    const void *password = NULL;
     if (pam_get_item(pamh, PAM_AUTHTOK, &password) == PAM_SUCCESS &&
         password) {
         return strdup((const char *) password);
